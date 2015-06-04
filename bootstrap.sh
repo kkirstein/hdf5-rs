3bb2846f8a7ad255bf794090f34a79b4f1b0482d
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-06-04 11:41:38 +0200

    workaround SSL firewall issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ apt-get -y install libhdf5-dev
 
 # install rust compiler
 #curl -sf -L https://static.rust-lang.org/rustup.sh | sh
-curl --silent -L https://static.rust-lang.org/rustup.sh -O
+curl --insecure --silent -L https://static.rust-lang.org/rustup.sh -O
 # this must be called interactively
 #sh rustup.sh --disable-sudo
 
